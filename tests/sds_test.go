bdd11da1c286f0c0a12d25b1c5865b3532c11f68
yujiang <lnsyyj@hotmail.com>
2018-09-24 03:04:15 +0800

    update
    Signed-off-by: yujiang <lnsyyj@hotmail.com>

--- tests/sds_test.go
+++ tests/sds_test.go
@@ -1,28 +1,33 @@
 package tests
 
 import (
-	"fmt"
-
 	"github.com/go-resty/resty"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ bool = Describe("Sds", func() {
+	var response = &resty.Response{}
+	BeforeEach(func() {
+		resp, err := resty.R().
+			SetHeaders(
+				map[string]string{
+					//"Content-Type": "application/json",
+					"LOG_USER":     "admin",
+					"X-Auth-Token": Token,
+				}).
+			Get(SDSPreURL + "softwareversion")
+		if err != nil {
+		}
+		response = resp
+	})
 
-	resp, err := resty.R().
-		SetHeaders(
-			map[string]string{
-				//"Content-Type": "application/json",
-				"LOG_USER":     "admin",
-				"X-Auth-Token": Token,
-			}).
-		Get(SDSPreURL + "softwareversion")
-	if err != nil {
-	}
-	fmt.Println(resp)
+	Describe("Categorizing book length", func() {
+		Context("The first book", func() {
 
-	It("should populate the fields correctly", func() {
-		Expect(resp.StatusCode()).To(Equal(200))
+			It("should populate the fields correctly", func() {
+				Expect(response.StatusCode()).To(Equal(200))
+			})
+		})
 	})
 })
